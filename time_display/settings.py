82107a9578b8b6c55e592a0500b749db2eb01e1e
Christie Caiano <59705609+ChristieGC13@users.noreply.github.com>
2021-03-16 15:02:44 -0400

    Update settings.py

--- time_display/settings.py
+++ time_display/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x7aibcttpl7n497q%7d1%=h_-ni$8u_yh##2+mzwyum+^t4pi@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
